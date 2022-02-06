90f16311e07bdb472e2f5da91722eca975492032
sergii-onyx <twilight.cympak@gmail.com>
2022-02-06 18:39:45 -0500

    testing concole.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! Javascript is running!")
+function fight() {
+    window.alert("The fight has began!");    
+}
+//fight();
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+// window.alert(playerName);
