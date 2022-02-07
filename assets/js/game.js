43652bc1991708bfdeb62676d0660e08718973bb
sergii-onyx <twilight.cympak@gmail.com>
2022-02-07 16:16:41 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,8 +67,15 @@ var fight = function(enemyName) {
 
 // run fight function to start game
 for (var i = 0; i < enemyNames.length; i++) {
+    if (playerName > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
-    fight(enemyNames[i]);
+    fight(pickedEnemyName);
 }
 
